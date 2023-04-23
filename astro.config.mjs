88346e48073b68d639ffbe75d8c14a741282a739
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-04-23 16:09:35 -0700

    adds sst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import aws from "astro-sst/lambda";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [mdx(), tailwind()],
+  output: "server",
+  integrations: [mdx(), tailwind()],
+  adapter: aws(),
 });
