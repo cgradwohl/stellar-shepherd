8c37de0a12a660a60833582361c73b337ee06f76
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-04-30 14:07:58 -0700

    save a bunch changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import aws from "astro-sst/lambda";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [mdx(), tailwind()],
-  adapter: aws(),
+  adapter: aws()
 });
